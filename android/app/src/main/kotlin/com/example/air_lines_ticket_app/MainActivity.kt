8abddb7eff07fae821afcbccd6881e28a54b0782
Hasib <hasibislam2k18@gmail.com>
2024-05-30 20:04:27 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/air_lines_ticket_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.air_lines_ticket_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/base/widget/app_double_text.dart
@@ -0,0 +1,33 @@
+import 'package:air_lines_ticket_app/base/res/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class AppDoubleText extends StatelessWidget {
+  final String bigText;
+  final String smallText;
+
+  const AppDoubleText(
+      {super.key, required this.bigText, required this.smallText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          bigText,
+          style: AppStyles.headLineStyle2,
+        ),
+        InkWell(
+          onTap: () {},
+          child: Text(
+            smallText,
+            style: AppStyles.textStyle.copyWith(
+              color: AppStyles.primaryColor
+
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
